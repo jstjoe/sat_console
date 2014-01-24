f7d210e1aadad2f46042974bda565607e3735da2
Joseph <joe@josephmccarron.com>
2014-01-23 20:10:15 -0800

    added rudimentary csv, page iteration

--- app.js
+++ app.js
@@ -11,11 +11,20 @@
       'getAllRatings.done':'parseRatings'
     },
     requests: {
-      getFilteredRatings: function(filter) {
-        return { url: '/api/v2/satisfaction_ratings.json?score=' + filter };
+      getFilteredRatings: function(filter, next_page_url) {
+        if (next_page_url) {
+          return { url: next_page_url + '?score=' + filter };
+        } else {
+          return { url: '/api/v2/satisfaction_ratings.json?score=' + filter };
+        }
+        
       },
-      getAllRatings: function(filter) {
-        return { url: '/api/v2/satisfaction_ratings.json' };
+      getAllRatings: function(filter, next_page_url) {
+        if (next_page_url) {
+          return { url: next_page_url };
+        } else {
+          return { url: '/api/v2/satisfaction_ratings.json' };
+        }
       }
     },
     //NAMED FUNCTIONS
@@ -37,25 +46,45 @@
       this.loadRatings(filter);
       //this.filter = filter;
     },
-    loadRatings: function(filter) {
+    loadRatings: function(filter, next_page_url) {
       if (filter == 'received' || filter == 'received_with_comment' || filter == 'received_without_comment' || filter == 'good' || filter == 'good_with_comment' || filter == 'good_without_comment' || filter == 'bad' || filter == 'bad_with_comment' || filter == 'bad_without_comment') {
         this.filter = filter;
-        console.log("Loading ratings... w/ " + filter);
-        this.ajax('getFilteredRatings', filter);
+        if (next_page_url) {
+          console.log("Loading next page of ratings...\n ...w/ filter: " + filter + "\n  ...and url: " + next_page_url);
+          this.ajax('getFilteredRatings', filter, next_page_url);
+        } else {
+          console.log("Loading ratings...\n ...w/ filter: " + filter + "\n  ...and no url(?) " + next_page_url);
+          this.ajax('getFilteredRatings', filter);
+        }
       } else{
-        this.filter = 'none';
-        console.log("Loading ratings... w/o a filter");
-        this.ajax('getAllRatings');
+        if (next_page_url) {
+          this.filter = 'none';
+          console.log("Loading next page of ratings...\n ...w/o a filter");
+          this.ajax('getAllRatings', next_page_url);
+        } else {
+          this.filter = 'none';
+          console.log("Loading ratings...\n ...w/o a filter");
+          this.ajax('getAllRatings');
+        }
       }
     },
     parseRatings: function(data) {
       var ratings = data.satisfaction_ratings;
-      console.log(ratings[0]);
-      this.ratings = ratings;
-      this.switchTo('list', {
+      if (data.previous_page===null && data.next_page) {
+        this.ratings = ratings;
+        this.loadRatings(this.filter, data.next_page);
+      } else if (data.previous_page && data.next_page && data.next_page != 'https://support.zendesk.com/api/v2/satisfaction_ratings.json?page=4') {
+        this.ratings = this.ratings.concat(ratings);
+        this.loadRatings(this.filter, data.next_page);
+      } else if (data.previous_page===null && data.next_page===null) {
+        this.ratings = ratings;
+      } else {
+        console.log("Stopping");
+        this.switchTo('csv', {
         ratings: this.ratings,
         filter: this.filter
       });
+      }
     }
   };
 
